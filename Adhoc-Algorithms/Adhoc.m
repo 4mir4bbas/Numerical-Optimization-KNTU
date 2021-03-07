1d31c0c4cc0a0bf2a310b943a924358909b2c218
Amir Abbas Bakhshipour <bakhshipour@email.kntu.ac.ir>
2021-03-07 14:33:20 +0330

    Last modifications
    1. iterativeGss() function added
    2. recirsiveGss() function and fibonacci function modified so that they return the number of iterations
    3. comments modified
    4. minor bugs fixed

--- Adhoc-Algorithms/Adhoc.m
+++ Adhoc-Algorithms/Adhoc.m
@@ -5,10 +5,11 @@ classdef Adhoc
     properties
         Function  % Objective funciton (single variable & unimodal)
         Period  % The period in which we want to find the minimum
-        rho_gss = 0.382;  % Fixed rho for GSS algorithm
-        rho_fibonacci = [];  % Variable rho for Fibonacci algorithm
-        fib = [];  % Fibonacci Sequence
-        
+        rhoGss = 0.382;  % Fixed rho for GSS algorithm
+        rhoFibonacci = [];  % Variable rho for Fibonacci algorithm
+        fib = [];  % Fibonacci sequence
+        epsilon = 0.05;  % Epsilon in Fibonacci algorithm
+        gssIter = -1;  % Number of iterations of GSS algorithm 
     end
     
     methods
@@ -21,11 +22,12 @@ classdef Adhoc
             % Example: 
             %           syms x;
             %           f=x^2;
-            %           period=[-1,1];
-            %           object = Adhoc(f,period);
+            %           p=[-1,1];
+            %           object = Adhoc(f,p);
             
             obj.Function = Function;
             obj.Period = Period;
+            % Generating Fibonacci sequence up to 100 instances
             obj.fib(1) = 1;
             obj.fib(2) = 2;
             for i=3:100
@@ -35,68 +37,136 @@ classdef Adhoc
         end
         
         
-        function lastPeriod = gss(obj, precision)    
+        function [periodStart, periodEnd, functionValue, gssIt] = recursiveGss(obj, precision)    
         
-            % GSS algorithm.
-            % This is a recursive implementation of GSS algorithm.
-            % It performs GSS algorithm on an Adhoc object.
-            % It accepts percision as input.
+            % Recursive implementation of GSS algorithm
+            % It performs GSS algorithm on an Adhoc object
+            % It accepts percision as input
+            % It returns the start and end of the final period and ...
+            % value of the function in average point of the last period ...
+            % and the number of iterations for convergence respectively
             %
             % Example:
             %           syms x;
             %           f=x^2;
-            %           period=[-1,1];
-            %           object = Adhoc(f,period);
-            %           finalPeriod = object.gss(0.1);
-        
+            %           p=[-1,1];
+            %           object = Adhoc(f,p);
+            %           [s,e,f,i] = object.recursiveGss(0.1);
+            
+            obj.gssIter = obj.gssIter + 1;
             length = obj.Period(2) - obj.Period(1);
             if length < precision
-                lastPeriod = obj.Period;
+                periodStart = obj.Period(1);
+                periodEnd = obj.Period(2);
+                functionValue = double(subs(obj.Function, (periodStart+periodEnd)/2));
+                gssIt = obj.gssIter;
             else
-                left = obj.Period(1) + obj.rho_gss * length;
-                right = obj.Period(2) - obj.rho_gss * length;
+                left = obj.Period(1) + obj.rhoGss * length;
+                right = obj.Period(2) - obj.rhoGss * length;
                 left_value = double(subs(obj.Function, left));
-                right_value = double(subs(obj.Function, right));
+                right_value = double(subs(obj.Function, right));     
                 if left_value < right_value
                     obj.Period = [obj.Period(1), right];
-                    lastPeriod = gss(obj, precision);
+                    [periodStart, periodEnd, functionValue, gssIt] = recursiveGss(obj, precision);
                 else
                     obj.Period = [left, obj.Period(2)];
-                    lastPeriod = gss(obj, precision);
+                    [periodStart, periodEnd, functionValue, gssIt] = recursiveGss(obj, precision);
                 end 
             end
         end
         
         
-        
-        function lastPeriod = fibonacci(obj, precision, epsilon)
-            
-            % Fibonacci algorithm
-            % It performs Fibonacci algorithm on an Adhoc object.
-            % It accepts percision and epsilon as input.
+        function [periodStart, periodEnd, functionValue, gssIt] = iterativeGss(obj, precision)
+           
+            % Iterative implementation of GSS algorithm
+            % It performs GSS algorithm on an Adhoc object
+            % It accepts percision as input
+            % It returns the start and end of the final period and ...
+            % value of the function in average point of the last period ...
+            % and the number of iterations for convergence respectively
             %
             % Example:
             %           syms x;
             %           f=x^2;
-            %           period=[-1,1];
-            %           object = Adhoc(f,period);
-            %           finalPeriod = object.fibonacci(0.5, 0.1);
+            %           p=[-1,1];
+            %           object = Adhoc(f,p);
+            %           [s,e,f,i] = object.iterativeGss(0.1);
+            
+            whichSide = 2;
+            ctr = 0;
+            while true
+                length = obj.Period(2) - obj.Period(1);
+                if length < precision
+                    break;
+                end
+                if whichSide == 2
+                    left = obj.Period(1) + obj.rhoGss * length;
+                    right = obj.Period(2) - obj.rhoGss * length;
+                    leftValue = double(subs(obj.Function, left));
+                    rightValue = double(subs(obj.Function, right));
+                else
+                    if ~whichSide
+                        left = obj.Period(1) + obj.rhoGss * length;
+                        leftValue = double(subs(obj.Function, left));
+                    else
+                        right = obj.Period(2) - obj.rhoGss * length;
+                        rightValue = double(subs(obj.Function, right));
+                    end
+
+                end
+                if leftValue < rightValue
+                    obj.Period = [obj.Period(1), right];
+                    whichSide = false;
+                    right = left;
+                    rightValue = leftValue;
+                else
+                    obj.Period = [left, obj.Period(2)];
+                    whichSide = true;
+                    left = right;
+                    leftValue = rightValue;
+                end
+                ctr = ctr + 1;
+            end
+            periodStart = obj.Period(1);
+            periodEnd = obj.Period(2);
+            functionValue = double(subs(obj.Function, (periodStart + periodEnd)/2));
+            gssIt = ctr;
+        end
+        
+        
+        function [periodStart, periodEnd, functionValue, fibIter] = fibonacci(obj, precision)
+            
+            % Fibonacci algorithm
+            % It performs Fibonacci algorithm on an Adhoc object.
+            % It accepts percision as input.
+            % It returns the start and end of the final period and ...
+            % value of the function in average point of the last period ...
+            % and the number of iterations for convergence respectively
+            % Note that the fixed epsilon is equal to 0.05 in properties
+            %
+            % Example:
+            %           syms x;
+            %           f=x^2;
+            %           p=[-1,1];
+            %           object = Adhoc(f,p);
+            %           [s,e,f,i] = object.fibonacci(0.3);
             
             length = obj.Period(2) - obj.Period(1);
             
             % finding N
             for i=1:100
-                if length*(1+2*epsilon)/obj.fib(i+1) <= precision
+                if length*(1+2*obj.epsilon)/obj.fib(i+1) < precision
                     N = i;
                     break;
                 end
             end
+            
             % Calculating rhos for each iteration
             for i=1:N
                 if i~=N
-                    obj.rho_fibonacci = [obj.rho_fibonacci 1-(obj.fib(N-i+1)/obj.fib(N-i+2))];
+                    obj.rhoFibonacci = [obj.rhoFibonacci 1-(obj.fib(N-i+1)/obj.fib(N-i+2))];
                 else
-                    obj.rho_fibonacci = [obj.rho_fibonacci 0.5 - epsilon];
+                    obj.rhoFibonacci = [obj.rhoFibonacci 0.5 - obj.epsilon];
                 end
             end
             
@@ -106,20 +176,25 @@ classdef Adhoc
                 if length < precision
                     break;
                 end
-                left = obj.Period(1) + obj.rho_fibonacci(i) * length;
-                right = obj.Period(2) - obj.rho_fibonacci(i) * length;
-                left_value = double(subs(obj.Function, left));
+                
+                left = obj.Period(1) + obj.rhoFibonacci(i) * length;
+                right = obj.Period(2) - obj.rhoFibonacci(i) * length;
                 right_value = double(subs(obj.Function, right));
+                left_value = double(subs(obj.Function, left));
+                
                 if left_value < right_value
                     obj.Period = [obj.Period(1), right];
                 else
                     obj.Period = [left, obj.Period(2)];
                 end        
             end
-            % Final Period
-            lastPeriod = obj.Period;   
+            
+            % Results
+            periodStart = obj.Period(1);
+            periodEnd = obj.Period(2);
+            functionValue = double(subs(obj.Function, (periodStart+periodEnd)/2));
+            fibIter = N;
         end
     end
     
 end
-
